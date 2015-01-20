10f3a9f106f768b75a23aad8f280cf39e65f84ba
Aryan Naraghi <aryan.naraghi@gmail.com>
2015-01-20 13:20:08 -0800

    Adds the template for the canvas.

--- collabpaint.py
+++ collabpaint.py
@@ -1,11 +1,23 @@
+import os
+
+import jinja2
 import webapp2
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(
+        os.path.join(os.path.dirname(__file__), 'templates')),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
 
 class Canvas(webapp2.RequestHandler):
 
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        template_values = {
+
+        }
+        template = JINJA_ENVIRONMENT.get_template('canvas.html')
+        self.response.write(template.render(template_values))
 
 
 application = webapp2.WSGIApplication([
