aba6ea32b4415edfc4939ca41cc53f15691e459f
Aryan Naraghi <aryan.naraghi@gmail.com>
2015-01-20 17:29:25 -0800

    Adds support for drawing the existing line segments when the app is loaded.

--- collabpaint.py
+++ collabpaint.py
@@ -19,19 +19,27 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 
-def GenerateClientId():
-    return random.randrange(- 2 ** 63, 2 ** 63 - 1)
+def GenerateClientId(room_name):
+    return room_name + str(random.randrange(- 2 ** 63, 2 ** 63 - 1))
 
 
 class Room(ndb.Model):
-    client_ids = ndb.IntegerProperty(repeated=True)
+    client_ids = ndb.StringProperty(repeated=True)
 
 
 class LineSegments(ndb.Model):
-    room = ndb.KeyProperty(kind=Room)
+    date_time = ndb.DateTimeProperty(auto_now_add=True)
     xs = ndb.IntegerProperty(repeated=True)
     ys = ndb.IntegerProperty(repeated=True)
     user = ndb.UserProperty()
+
+    def GetPoints(self):
+        return zip(self.xs, self.ys)
+
+    @classmethod
+    def GetLineSegmentsForRoom(cls, room_key):
+        return LineSegments.query(
+            ancestor=ndb.Key(Room, room_key)).order(LineSegments.date_time)
 
 
 class Canvas(webapp2.RequestHandler):
@@ -44,11 +52,11 @@ class Canvas(webapp2.RequestHandler):
 
         # TODO(aryann): Check the query param "room_key" and use it here.
         room = Room.get_or_insert(DEFAULT_ROOM)
-        client_id = GenerateClientId()
+        client_id = GenerateClientId(DEFAULT_ROOM)
         room.client_ids.append(client_id)
         room.put()
 
-        token = channel.create_channel(room.key.string_id() + str(client_id))
+        token = channel.create_channel(client_id)
         template_values = {
             'room_key': room.key.string_id(),
             'width': constants.WIDTH,
@@ -59,7 +67,18 @@ class Canvas(webapp2.RequestHandler):
         self.response.write(template.render(template_values))
 
 
-class CanvasUpdater(webapp2.RequestHandler):
+class LinesHandler(webapp2.RequestHandler):
+
+    def get(self):
+        room_key = self.request.get('room_key')
+        print room_key
+        client_id = self.request.get('client_id')
+
+        lines = []
+        for line_segment in LineSegments.GetLineSegmentsForRoom(
+            room_key).fetch():
+            lines.append(line_segment.GetPoints())
+        channel.send_message(client_id, json.dumps(lines))
 
     def post(self):
         room_key = self.request.get('room_key')
@@ -72,7 +91,7 @@ class CanvasUpdater(webapp2.RequestHandler):
         xs, ys = zip(*line_segments)
 
         line_segments = LineSegments(
-            room=ndb.Key(Room, room_key),
+            parent=ndb.Key(Room, room_key),
             xs=xs,
             ys=ys,
             user=user)
@@ -80,11 +99,11 @@ class CanvasUpdater(webapp2.RequestHandler):
 
         room = Room.get_by_id(room_key)
         for client_id in room.client_ids:
-            channel.send_message(room_key + str(client_id),
-                                 self.request.body)
+            channel.send_message(
+                client_id, json.dumps([line_segments.GetPoints()]))
 
 
 application = webapp2.WSGIApplication([
         ('/', Canvas),
-        ('/update', CanvasUpdater),
+        ('/lines', LinesHandler),
 ], debug=True)
